5ef0cf700198f693a77e6a8caff421d8f544ba9f
luy319 <luyandadelight@gmail.com>
2025-09-29 21:23:46 +0000

    change new

--- HealthKiosk.java
+++ HealthKiosk.java
@@ -125,7 +125,6 @@ public class HealthKiosk {
                 System.out.println("Summary: COUNSELING | ID=" + shortId + " | Code=" + displayCode);
                 break;
         }
-
         
     }
 }
